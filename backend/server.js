c99c431e43796973b61301b38463daecf643a178
shagaranasution <shagaranst@gmail.com>
2025-06-24 18:36:27 +0700

    Setup one domain to serve both backend and frontend service

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
-import express, { json, urlencoded } from 'express';
+import express, { json, urlencoded, static as static_ } from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import productRoute from './routes/product.route.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(json()); // Middleware that allows us to accept JSON data in req.body
 app.use(urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/products', productRoute);
+app.use('/api/products', productRoutes);
 
-app.listen(3000, () => {
+// Production: serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(static_(path.join(__dirname, 'frontend', 'dist')));
+  app.get('/*splat', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
   connectDB();
-  console.log('Server is running on port 3000...');
+  console.log('Server is started at http://localhost:' + PORT);
 });
